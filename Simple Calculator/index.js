1f92d5e08f2bb6b6b9c97189d12942548391bb59
Ammara Aslam <ammaraaslam.work@gmail.com>
2023-04-18 11:06:27 -0700

    calculator with basic airthmatic operators

---
+++ Simple Calculator/index.js
@@ -0,0 +1,10 @@
+let num1El = document.getElementById("num1")
+let num2El = document.getElementById("num2")
+let resultEL = document.getElementById("result")
+function add()
+{
+    resultEL.textContent= num1El.value + num2El.value
+}
+let a = 2
+let b = 3
+console.log(a+b)
